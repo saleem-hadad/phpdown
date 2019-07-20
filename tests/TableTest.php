c4009ac9a89029af307883775ba5d0e91e312d10
Saleem Hadad <saleem_ha@hotmail.com>
2019-07-20 11:55:02 +0400

    wip

--- tests/TableTest.php
+++ tests/TableTest.php
@@ -11,7 +11,7 @@ class TableTest extends TestCase
     /** @test */
     public function it_renders_correctly()
     {
-        $tableHeader = Table::make([
+        $table = Table::make([
             'col1' => 'center',
             'col2' => 'left',
             'col3' => 'right',
@@ -19,6 +19,6 @@ class TableTest extends TestCase
             TableRow::make(['data', 'data', 'data']),
         ]);
 
-        $this->assertEquals("| col1 | col2 | col3 | \n| :-: | :- | -: | \n| data | data | data | \n", $tableHeader->render());
+        $this->assertEquals("| col1 | col2 | col3 | \n| :-: | :- | -: | \n| data | data | data | \n", $table->render());
     }
 }
